b021727915d8a1ecd3aaf7f1795042017c264817
Tom Rains <tomrainswrites@gmail.com>
2019-06-22 19:33:38 -0500

    Updates

---
+++ precache-manifest.6ff28b1844ba0252e6ed7b255fb2e579.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d4cf3efcb61a5a8588b9e523254380",
+    "url": "/ZenDo/index.html"
+  },
+  {
+    "revision": "92c11d650faecb295796",
+    "url": "/ZenDo/static/css/main.ab532436.chunk.css"
+  },
+  {
+    "revision": "ff855dfb89af0ea170c5",
+    "url": "/ZenDo/static/js/2.f90bcaa9.chunk.js"
+  },
+  {
+    "revision": "92c11d650faecb295796",
+    "url": "/ZenDo/static/js/main.fe8a1ba5.chunk.js"
+  },
+  {
+    "revision": "9c8df1912d24b8ac2a91",
+    "url": "/ZenDo/static/js/runtime~main.71f6aa32.js"
+  },
+  {
+    "revision": "48f7554b73c0eea073abed0855f681d9",
+    "url": "/ZenDo/static/media/001-aromatherapy.48f7554b.png"
+  },
+  {
+    "revision": "b7b8c13f9a40388ab7fffaf057fa66bf",
+    "url": "/ZenDo/static/media/002-aroma.b7b8c13f.png"
+  },
+  {
+    "revision": "f38c5b660f71ddce14785510b974fb45",
+    "url": "/ZenDo/static/media/004-aroma.f38c5b66.png"
+  },
+  {
+    "revision": "89dafc41cb29b0fa76e21252c9767233",
+    "url": "/ZenDo/static/media/005-bamboo.89dafc41.png"
+  },
+  {
+    "revision": "3ca826901325732116b9ae0c1560e8ee",
+    "url": "/ZenDo/static/media/006-bathrobe.3ca82690.png"
+  },
+  {
+    "revision": "988f583d626c4f4cbe7edbb9e6ae7d2b",
+    "url": "/ZenDo/static/media/007-sauna.988f583d.png"
+  },
+  {
+    "revision": "b310f797e016843329a0122269944e0b",
+    "url": "/ZenDo/static/media/008-candle.b310f797.png"
+  },
+  {
+    "revision": "ee6a5b2214dc67fc140a8b305b010f41",
+    "url": "/ZenDo/static/media/012-water.ee6a5b22.png"
+  },
+  {
+    "revision": "424adb97f69f73b3548dc5cd8bd8eaea",
+    "url": "/ZenDo/static/media/013-herb.424adb97.png"
+  },
+  {
+    "revision": "5a66d86ebc24abb5535d8ad7f4653702",
+    "url": "/ZenDo/static/media/014-herb.5a66d86e.png"
+  },
+  {
+    "revision": "f7ac8d9ef02b95e18987f588ad436406",
+    "url": "/ZenDo/static/media/016-incense.f7ac8d9e.png"
+  },
+  {
+    "revision": "1e616787d1b56eb7fa6417c7447d44d2",
+    "url": "/ZenDo/static/media/017-lotus flower.1e616787.png"
+  },
+  {
+    "revision": "cc0cd73c5e16edf6f094868b373502c5",
+    "url": "/ZenDo/static/media/022-mortar.cc0cd73c.png"
+  },
+  {
+    "revision": "d32fbaf3f7c25dc9aac1fb5e3f74a8f4",
+    "url": "/ZenDo/static/media/024-salt.d32fbaf3.png"
+  },
+  {
+    "revision": "9225671022be6192207c2684f9f60740",
+    "url": "/ZenDo/static/media/031-spa.92256710.png"
+  },
+  {
+    "revision": "c03f0fee7b4986c224ee8dd2ab764d15",
+    "url": "/ZenDo/static/media/033-spa.c03f0fee.png"
+  },
+  {
+    "revision": "52b23260c3352e0714677cdab2e6926b",
+    "url": "/ZenDo/static/media/034-stone.52b23260.png"
+  },
+  {
+    "revision": "9bd04e0dc4f5219c444e7bde27f09377",
+    "url": "/ZenDo/static/media/035-teapot.9bd04e0d.png"
+  },
+  {
+    "revision": "8bbcf97368ad2bd9a108f7270b0c0755",
+    "url": "/ZenDo/static/media/036-towels.8bbcf973.png"
+  }
+]);
